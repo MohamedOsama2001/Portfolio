c432caf5e8f3ee4714bd4589a803762789e34226
MohamedOsama2001 <ma8556432@gmail.com>
2025-01-05 02:55:37 +0200

    add services section

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 function writing() {
   const dynamicText = document.querySelector("#dynamic-text");
   const myText = [
-    "FullStack Web Developer.",
     "Frontend Web Developer.",
-    "Backend Web Developer.",
+    "Passionate about build interactive web application.",
   ];
   let textIndex = 0;
   let charIndex = 0;
@@ -12,7 +11,7 @@ function writing() {
     if (charIndex < myText[textIndex].length) {
       dynamicText.innerHTML = myText[textIndex].substring(0, charIndex + 1);
       charIndex++;
-      setTimeout(writeChar, 200);
+      setTimeout(writeChar, 100);
     } else {
       setTimeout(delChar, 1400);
     }
@@ -33,18 +32,15 @@ writing();
 //////////////////////////////////////////////////////////////////////////////////////
 //skills
 const skills = [
-  "HTML",
-  "CSS",
+  "HTML&CSS",
   "HTML5",
   "CSS3",
   "Bootstrap",
   "JS&ES6",
   "React",
+  "TypeScript",
   "Git&GitHub",
   "APIs&Axios",
-  "Node.js",
-  "Express.js",
-  "Mongodb",
 ];
 const chunkSkills = (array, size) => {
   const chunks = [];
@@ -108,34 +104,38 @@ let projects = [
   {
     id: "1",
     name: "E-Commerce",
-    descrp: "Based on React.js & Redux",
+    descrp: "Based on React Js & Redux",
     image: "./images/project10.jpg",
     codeLink: "https://github.com/MohamedOsama2001/E-Commerce",
     demoLink: "https://e-commerce-liard-xi-49.vercel.app/",
+    tools: "React",
   },
   {
-    id: "3",
+    id: "2",
     name: "Dynamic weather app",
     descrp: "Based on React JS",
     image: "./images/project7.jpg",
     codeLink: "https://github.com/MohamedOsama2001/weather-app",
     demoLink: "https://weather-app-opal-psi.vercel.app/",
+    tools: "React",
   },
   {
     id: "3",
     name: "ShoppingCart",
-    descrp: "Based on Java Script",
+    descrp: "Based on JavaScript",
     image: "./images/project4.jpg",
     codeLink: "https://github.com/MohamedOsama2001/online-shopping",
     demoLink: "https://mohamedosama2001.github.io/online-shopping/",
+    tools: "JavaScript",
   },
   {
     id: "4",
     name: "To do List",
-    descrp: "Based on Java Script",
+    descrp: "Based on JavaScript",
     image: "./images/project6.jpg",
     codeLink: "https://github.com/MohamedOsama2001/To-Do-List",
     demoLink: "https://mohamedosama2001.github.io/To-Do-List/",
+    tools: "JavaScript",
   },
   {
     id: "5",
@@ -144,6 +144,7 @@ let projects = [
     image: "./images/project3.jpg",
     codeLink: "https://github.com/MohamedOsama2001/Egypt-Tourism",
     demoLink: "https://mohamedosama2001.github.io/Egypt-Tourism/",
+    tools: "Bootstrap",
   },
   {
     id: "6",
@@ -152,6 +153,7 @@ let projects = [
     image: "./images/project2.jpg",
     codeLink: "https://github.com/MohamedOsama2001/2nd_frontend_project",
     demoLink: "https://mohamedosama2001.github.io/2nd_frontend_project/",
+    tools: "HTML5&CSS3",
   },
   {
     id: "7",
@@ -160,6 +162,7 @@ let projects = [
     image: "./images/project5.jpg",
     codeLink: "https://github.com/MohamedOsama2001/LandingPage/",
     demoLink: "https://mohamedosama2001.github.io/LandingPage/",
+    tools: "HTML5&CSS3",
   },
   {
     id: "8",
@@ -168,9 +171,21 @@ let projects = [
     image: "./images/project1.png",
     codeLink: "https://github.com/MohamedOsama2001/FirstFrontendProject",
     demoLink: "https://mohamedosama2001.github.io/FirstFrontendProject/",
+    tools: "HTML&CSS",
   },
 ];
+let toolsUSed = ["HTML&CSS", "HTML5&CSS3", "Bootstrap", "JavaScript", "React"];
 let projectDiv = document.querySelector(".projects-content .row");
+let filterButtons = document.querySelector(".filter-butttons");
+const drawButtons = (buttons) => {
+  let button = buttons.map((item) => {
+    return `
+    <button class='btn btn-outline-light filter-button' data-tool=${item}>${item}</button>
+    `;
+  });
+  filterButtons.innerHTML += button.join("");
+};
+drawButtons(toolsUSed);
 function drawProjects(projects) {
   let proj = projects.map((item) => {
     return `
@@ -192,7 +207,7 @@ function drawProjects(projects) {
                       href=${item.demoLink}
                       target="_blank"
                     >
-                      <i class="fas fa-globe"></i> Visit
+                      <i class="fas fa-link"></i> Link
                     </a>
                   </div>
                 </div>
@@ -202,3 +217,21 @@ function drawProjects(projects) {
   projectDiv.innerHTML = proj.join("");
 }
 drawProjects(projects);
+const allProjectsBtn=document.querySelector('.all-btn')
+allProjectsBtn.onclick = ()=> drawProjects(projects)
+//////////////////////////////////////////////////////
+//filter function
+const filterProjects = (tool) => {
+  const filteredProjects = projects.filter((item) => item.tools === tool);
+  drawProjects(filteredProjects);
+};
+const addFilterEventListeners = () => {
+  const filterButtons = document.querySelectorAll(".filter-button");
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const tool = button.getAttribute("data-tool");
+      filterProjects(tool);
+    });
+  });
+};
+addFilterEventListeners();
